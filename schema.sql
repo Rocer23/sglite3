523256259431a4f6c7e8f05d5d21d87d38c9e2bf
Rocer23 <139039560+Rocer23@users.noreply.github.com>
2023-08-02 16:37:52 +0300

    data base
    Create data base

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "pupils" (
+	"id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	"avg_score"	INTEGER NOT NULL,
+	"class"	TEXT NOT NULL,
+	"parents"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
